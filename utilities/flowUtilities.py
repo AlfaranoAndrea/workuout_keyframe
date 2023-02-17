f63016bf94938fc0186ed6cb4614abffe999cd79
AlfaranoAndrea <103185958+AlfaranoAndrea@users.noreply.github.com>
2023-02-17 12:07:31 +0100

    Update flowUtilities.py

--- utilities/flowUtilities.py
+++ utilities/flowUtilities.py
@@ -16,17 +16,13 @@ def computeFlow(img1, img2, model, device):
     img1=torch.reshape(img1,(1, c, h,w))
     img2=torch.reshape(img2,(1, c, h,w))
 
-    # x = torch.cat([img1,img2],1)
-    y=model(img1,img2)
+    x = torch.cat([img1,img2],1)
+    y=model(x)
     y_numpy=y[0][0].cpu().detach().numpy()
     y2=np.transpose (y_numpy,(1,2,0))
     flow=fz.convert_from_flow(y2) #i use flowviz to convert flow to image
     return flow
 
-def _eightDivisibility(number):
-    while number%8 != 0:
-        number+=2
-    return number
 
 def flowVideo(path, model, device,step=4,temporary_path= "./temp/processedFloFrames/"):
     os.makedirs(temporary_path, exist_ok=True)
@@ -34,18 +30,13 @@ def flowVideo(path, model, device,step=4,temporary_path= "./temp/processedFloFra
     files.sort(key = lambda x: int(x[:-4]))
     
     image2 = cv2.imread(path+files[0])
-
     scale_percent = 40 # percent of original size
     width = int(image2.shape[1] * scale_percent / 100)
     height = int(image2.shape[0] * scale_percent / 100)
-    dim = (
-        _eightDivisibility(width), 
-        _eightDivisibility(height)
-        )
-    image2 = cv2.resize(image2, dim, interpolation = cv2.INTER_AREA)
+    image2 = cv2.resize(image2, (width, height), interpolation = cv2.INTER_AREA)
     for i in tqdm(range(1,len(files))[::step]):
         image1=image2
         image2 = cv2.imread(path+files[i])
-        image2 = cv2.resize(image2, dim, interpolation = cv2.INTER_AREA)
+        image2 = cv2.resize(image2, (width, height), interpolation = cv2.INTER_AREA)
         frame=computeFlow(image1,image2,model, device)
         cv2.imwrite(f"{temporary_path}{i}.jpg", frame)
